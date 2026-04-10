8d506d0a2a325eb8f40282de8ad955d1422d419b
JR <zorael@gmail.com>
2026-04-10 12:49:49 +0200

    Move Println backend into own println.rs

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,4 +1,7 @@
-use crate::compose;
+mod println;
+
+pub use println::PrintlnBackend;
+
 use crate::context;
 
 pub trait Backend {
@@ -11,45 +14,4 @@ pub trait Backend {
     fn compose_startup_failed(&self, ctx: &context::Context) -> String;
 
     fn emit(&self, message: &str) -> Result<Option<String>, String>;
-}
-
-pub struct PrintlnBackend {
-    pub id: usize,
-    pub name: String,
-}
-
-impl PrintlnBackend {
-    pub fn new(id: usize, name: &str) -> Self {
-        Self {
-            id,
-            name: name.to_string(),
-        }
-    }
-}
-
-impl Backend for PrintlnBackend {
-    fn id(&self) -> usize {
-        self.id
-    }
-
-    fn name(&self) -> &str {
-        &self.name
-    }
-
-    fn compose_alert(&self, ctx: &context::Context) -> String {
-        compose::compose_alert_message(ctx)
-    }
-
-    fn compose_reminder(&self, ctx: &context::Context) -> String {
-        compose::compose_reminder_message(ctx)
-    }
-
-    fn compose_startup_failed(&self, ctx: &context::Context) -> String {
-        compose::compose_startup_failed_message(ctx)
-    }
-
-    fn emit(&self, message: &str) -> Result<Option<String>, String> {
-        println!("emit: {message}");
-        Ok(None)
-    }
 }
